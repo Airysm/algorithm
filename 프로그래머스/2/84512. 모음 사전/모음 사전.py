971a6a0a63b7a31778856054057d607d8867c12c
Airysm <shwon775@naver.com>
2024-02-15 15:27:36 +0900

    [level 2] Title: 모음 사전, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,16 +1,9 @@
 def solution(word):
-    alphabet = "AEIOU"
-    num_list = []
     answer = 0
-    num = 0
-    
-    for i in range(5):
-        num += 5**i
-        num_list.append(num)
-    num_list = num_list[::-1]
+    alphabet = "AEIOU"
+    num_list = [781, 156, 31, 6, 1]
     
     for i in range(len(word)):
-        answer += num_list[i] * alphabet.find(word[i])
-        answer += 1
+        answer += num_list[i] * alphabet.find(word[i]) + 1
                 
     return answer
