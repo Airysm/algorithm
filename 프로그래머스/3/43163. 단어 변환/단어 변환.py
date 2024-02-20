4ce6cb2bf09148e2f7d0ce8655ac5e8ed5d22fc8
Airysm <shwon775@naver.com>
2024-02-20 14:57:39 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,33 +1,26 @@
-visited = []
-result = []
-
-def dfs(begin, target, words, count):
-    if begin == target:
-        result.append(count)
-        return
-    
-    for i in range(len(words)):
-        if visited[i] == 1:
-            continue
-        if word_diff(begin, words[i]) == 1:
-            visited[i] = 1
-            dfs(words[i], target, words, count+1)
-            visited[i] = 0
-
-def word_diff(begin, target):
-    diff = 0
-    for i in range(len(begin)):
-            if begin[i] != target[i]:
-                diff += 1
-    return diff
+from collections import deque
 
 def solution(begin, target, words):
-    for _ in range(len(words)):
-        visited.append(0)
-    
     if target not in words:
         return 0
-        
-    dfs(begin, target, words, 0)
     
-    return min(result) 
+    return bfs(begin, target, words)
+
+def bfs(begin, target, words):
+    queue = deque()
+    queue.append([begin, 0])
+    
+    while queue:
+        print(queue)
+        word, count = queue.popleft()
+        if word == target:
+            return count
+        
+        for w in words:
+            diff = 0
+            for i in range(len(word)):
+                if word[i] != w[i]:
+                    diff += 1
+            
+            if diff == 1:
+                queue.append([w, count+1])
