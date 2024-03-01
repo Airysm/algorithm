191ac7e3fe802b044742e18fa1421ca270a68a2d
Airysm <shwon775@naver.com>
2024-03-01 14:21:28 +0900

    [level 3] Title: 기지국 설치, Time: 2.95 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,18 +1,12 @@
+import math
+
 def solution(n, stations, w):
     answer = 0
     now = 1
     
     for station in stations:
-        length = station - w - now
-        if length > 0:
-            answer += length//(w*2+1) 
-            if length%(w*2+1):
-                answer += 1
+        answer += math.ceil((station-w-now)/(w*2+1))
         now = station + w + 1
-    
-    if n - now + 1 > 0:
-        answer += (n-now+1)//(w*2+1)
-        if (n-now+1)%(w*2+1):
-            answer += 1
+    answer += math.ceil((n-now+1)/(w*2+1))
             
     return answer
