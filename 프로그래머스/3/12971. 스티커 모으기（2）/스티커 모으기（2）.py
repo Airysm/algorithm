e4bded66012159e2b464e949fdcd306a7f0aa1d5
Airysm <shwon775@naver.com>
2024-03-11 15:04:06 +0900

    [level 3] Title: 스티커 모으기(2), Time: 68.46 ms, Memory: 17.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,23 @@
+def solution(sticker):
+    answer = 0
+    dp  = [0 for _ in range(len(sticker))]
+    dp2 = [0 for _ in range(len(sticker))]
+    
+    if len(sticker) == 1:
+        return sticker[0]
+    elif len(sticker) == 2:
+        return max(sticker[0], sticker[1])
+    
+    dp[0] = sticker[0]
+    dp[1] = max(sticker[0], sticker[1])
+    for i in range(2, len(dp)-1):
+        dp[i] = sticker[i]
+        dp[i] = max(dp[i-2]+dp[i], dp[i-1])
+    
+    dp2[0] = 0
+    dp2[1] = sticker[1]
+    for i in range(2, len(dp)):
+        dp2[i] = sticker[i]
+        dp2[i] = max(dp2[i-2]+dp2[i], dp2[i-1])
+
+    return max(dp[-2], dp2[-1])
