fceac1e35f4245cc736b6cce5c11d2a28c1e4e11
Airysm <shwon775@naver.com>
2024-03-21 16:43:25 +0900

    [level 2] Title: 우박수열 정적분, Time: 3.09 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
@@ -0,0 +1,23 @@
+def solution(k, ranges):
+    answer = []
+    list_k = []
+    count = 0
+    
+    while k > 1:
+        temp = k
+        if k%2 == 0:    k = k//2
+        else:           k = k*3+1
+        list_k.append((temp+k)/2)
+        count += 1
+    
+    for r1, r2 in ranges:
+        if r2 <= 0:
+            r2 = r2+count
+        if r1 == r2:
+            answer.append(0.0)
+        elif r1 > r2:
+            answer.append(-1.0)
+        else:
+            answer.append(sum(list_k[r1:r2]))
+        
+    return answer
