a02c856b19d3d17d9128e59a204b172251eb5d38
Airysm <shwon775@naver.com>
2024-03-29 14:07:28 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,30 +1,19 @@
 import math
 
-def div_card(arrayA, arrayB):
-    arrayA.sort()
-    cdA = []
-    
-    for i in range(1, int(math.sqrt(arrayA[0]))):
-        if arrayA[0]%i == 0:
-            if i > 1:
-                cdA.append(i)
-            cdA.append(arrayA[0]//i)
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a%b)
 
-    cdA.sort(reverse=True)
+def div_card(arrayA, arrayB):
+    gcd_a = arrayA[0]
     
-    for i in cdA:
-        flag = True
-        for a in arrayA:
-            if a%i != 0:
-                flag = False
-                break
-        for b in arrayB:
-            if b%i == 0:
-                flag = False
-                break
-        if flag:
-            return i
-    return 0
+    for a in arrayA:
+        gcd_a = gcd(a, gcd_a)
+    for b in arrayB:
+        if b%gcd_a == 0:
+            return 0
+    return gcd_a
 
 def solution(arrayA, arrayB):
     return max(div_card(arrayA, arrayB), div_card(arrayB, arrayA))
