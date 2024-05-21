8928e7104a0a7be32fa87d78206ad2b132f3828c
Airysm <shwon775@naver.com>
2024-05-21 15:45:48 +0900

    [level 2] Title: 올바른 괄호, Time: 22.87 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 53.1 MB, 시간: 19.41 ms
+메모리: 52.8 MB, 시간: 22.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 15일 16:11:38
+2024년 05월 21일 15:45:47
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,16 +4,20 @@ class Solution {
     boolean solution(String s) {
         Stack<Character> st = new Stack<>();
         
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '(')
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
                 st.push('(');
-            else {
-                if(st.isEmpty())
+            else if (s.charAt(i) == ')') {
+                if (st.isEmpty())
                     return false;
-                st.pop();
+                else if (!st.isEmpty() && st.peek() == '(')
+                    st.pop();
             }
         }
         
-        return st.isEmpty();
+        if (!st.isEmpty())
+            return false;
+
+        return true;
     }
 }
