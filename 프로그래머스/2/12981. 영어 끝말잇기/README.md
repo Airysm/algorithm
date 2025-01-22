307ddc83eb4316403fde78d3875c2338ce42f2c2
Seo DongGwan <shwon775@naver.com>
2025-01-22 20:00:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.40 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 0.06 ms
+메모리: 88.6 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 25일 13:54:28
+2025년 01월 22일 20:00:22
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,20 +2,26 @@ import java.util.HashMap;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {0, 0};
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        map.put(words[0], 1);
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        int[] answer = new int[2];
+        int i;
         
-        for (int i = 1; i < words.length; i++) {
-            if (map.containsKey(words[i]) || words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
-                answer[0] = i%n+1;
-                answer[1] = i/n+1;
+        map.put(0, words[0]);
+        for (i = 1; i < words.length; i++) {
+            String w = words[i];
+            if (map.containsValue(w) || (w.charAt(0) != words[i-1].charAt(words[i-1].length()-1)))
                 break;
-            }
-            else
-                map.put(words[i], 1);
+            map.put(i, w);
         }
-
+        
+        if (map.size() == words.length) {
+            answer[0] = 0;
+            answer[1] = 0;
+        } else {
+            answer[0] = i%n+1;
+            answer[1] = i/n+1;
+        }
+        
         return answer;
     }
 }
